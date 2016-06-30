a74732dffbae90084a36aefa5215f1b87b9ae6a3
igeligel <kevinigeligeligel@gmail.com>
2016-06-30 23:31:23 +0200

    feat(factories)
    add factories for requests and encrypting passwords

--- src/skadisteam.login/Factories/EncryptPasswordFactory.cs
+++ src/skadisteam.login/Factories/EncryptPasswordFactory.cs
@@ -1,17 +1,36 @@
+using skadi_steam_login.Extensions;
 using skadi_steam_login.Models;
 using skadi_steam_login.Models.Json;
+using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace skadi_steam_login.Factories
 {
     public static class EncryptPasswordFactory
     {
-        public static EncryptPasswordModel Create(GetRsaKeyResponse getRsaKeyResponse, string password)
+        public static string Create(GetRsaKeyResponse getRsaKeyResponse, string password)
         {
             EncryptPasswordModel encryptPasswordModel = new EncryptPasswordModel();
             encryptPasswordModel.PublicKeyExp = getRsaKeyResponse.PublicKeyExp;
             encryptPasswordModel.PublicKeyMod = getRsaKeyResponse.PublicKeyMod;
             encryptPasswordModel.Password = password;
-            return encryptPasswordModel;
+            return EncryptPassword(encryptPasswordModel);
+        }
+
+        private static string EncryptPassword(EncryptPasswordModel encryptPasswordModel)
+        {
+            var rsa = new RSACryptoServiceProvider();
+
+            var rsaParameters = new RSAParameters
+            {
+                Exponent = encryptPasswordModel.PublicKeyExp.HexToByte(),
+                Modulus = encryptPasswordModel.PublicKeyMod.HexToByte()
+            };
+            rsa.ImportParameters(rsaParameters);
+            var bytePassword = Encoding.ASCII.GetBytes(encryptPasswordModel.Password);
+            var encodedPassword = rsa.Encrypt(bytePassword, false);
+            return Convert.ToBase64String(encodedPassword);
         }
     }
 }
---
+++ src/skadisteam.login/Factories/RequestFactory.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using skadi_steam_login.Constants;
+using skadi_steam_login.Http.Headers;
+using HttpMethod = skadi_steam_login.Http.HttpMethod;
+
+namespace skadi_steam_login.Factories
+{
+    public static class RequestFactory
+    {
+        public static HttpResponseMessage Create(HttpMethod method, Uri uri,
+            string path, Accept acceptHeader, string acceptLanguage,
+            bool upgradeInsecureRequests, bool steamCommunityOriginSet,
+            bool steamCommunityRefererSet, bool isXmlHttpRequest,
+            bool cacheControlSet,
+            IEnumerable<KeyValuePair<string, string>> postContent,
+            CookieContainer cookieContainer)
+        {
+            HttpResponseMessage response = null;
+            var handler = new HttpClientHandler();
+            handler.CookieContainer = cookieContainer;
+            handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = uri;
+                client.DefaultRequestHeaders.Add(HttpHeaderKeys.AcceptEncoding, "gzip, deflate");
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", acceptLanguage);
+                client.DefaultRequestHeaders.Add(HttpHeaderKeys.UserAgent, "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36");
+                client.DefaultRequestHeaders.Host = uri.Host;
+
+                if (acceptHeader == Accept.All)
+                {
+                    client.DefaultRequestHeaders.TryAddWithoutValidation(HttpHeaderKeys.Accept, "*/*");
+                }
+                else if (acceptHeader == Accept.Html)
+                {
+                    client.DefaultRequestHeaders.TryAddWithoutValidation(HttpHeaderKeys.Accept, "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                }
+
+                if (steamCommunityOriginSet)
+                {
+                    client.DefaultRequestHeaders.Add(HttpHeaderKeys.Origin, Ressources.SteamCommmunitySecureBase);
+                }
+
+                if (cacheControlSet)
+                {
+                    client.DefaultRequestHeaders.Add(HttpHeaderKeys.CacheControl, "max-age=0");
+                }
+
+                if (isXmlHttpRequest)
+                {
+                    client.DefaultRequestHeaders.Add(HttpHeaderKeys.XRequestedWith, "XMLHttpRequest");
+                }
+
+                if (steamCommunityRefererSet)
+                {
+                    client.DefaultRequestHeaders.Add(HttpHeaderKeys.Referer, "https://steamcommunity.com/login/home/?goto=0");
+                }
+
+                if (upgradeInsecureRequests)
+                {
+                    client.DefaultRequestHeaders.Add(HttpHeaderKeys.UpgradeInsecureRequests, "1");
+                }
+
+                if (method == HttpMethod.POST)
+                {
+                    var content = new FormUrlEncodedContent(postContent);
+                    response = client.PostAsync(path, content).Result;
+                }
+                else if (method == HttpMethod.GET)
+                {
+                    response = client.GetAsync(path).Result;
+                }
+
+            }
+            return response;
+        }
+    }
+}

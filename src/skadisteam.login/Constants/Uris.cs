abf65ba6b8126a4d139e48f10e43d8bdfebf0130
igeligel <kevinigeligeligel@gmail.com>
2016-06-30 23:31:00 +0200

    feat(uris)
    add uris for help.steampowered and steamcommunitybase

--- src/skadisteam.login/Constants/Uris.cs
+++ src/skadisteam.login/Constants/Uris.cs
@@ -4,6 +4,8 @@ namespace skadi_steam_login.Constants
 {
     internal class Uris
     {
+        public static Uri SteamCommunityBase = new Uri(Ressources.SteamCommmunityBase);
         public static Uri SteamCommmunitySecureBase = new Uri(Ressources.SteamCommmunitySecureBase);
+        public static Uri HelpSteampoweredSecureBase = new Uri("https://help.steampowered.com");
     }
 }

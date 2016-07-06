6924414e8468e3671516a8290f9f9dac8f311e72
igeligel <kevinigeligeligel@gmail.com>
2016-07-06 22:15:49 +0200

    refactor(model)
    order properties alphabetically

--- src/skadisteam.login/Models/Json/TransferParameters.cs
+++ src/skadisteam.login/Models/Json/TransferParameters.cs
@@ -4,17 +4,17 @@ namespace skadisteam.login.Models.Json
 {
     public class TransferParameters
     {
-        [JsonProperty(PropertyName = "steamid")]
-        public string SteamId { get; set; }
-        [JsonProperty(PropertyName = "token")]
-        public string Token { get; set; }
         [JsonProperty(PropertyName = "auth")]
         public string Auth { get; set; }
         [JsonProperty(PropertyName = "remember_login")]
         public bool RememberLogin { get; set; }
-        [JsonProperty(PropertyName = "webcookie")]
-        public string WebCookie { get; set; }
+        [JsonProperty(PropertyName = "steamid")]
+        public string SteamId { get; set; }
+        [JsonProperty(PropertyName = "token")]
+        public string Token { get; set; }
         [JsonProperty(PropertyName = "token_secure")]
         public string TokenSecure { get; set; }
+        [JsonProperty(PropertyName = "webcookie")]
+        public string WebCookie { get; set; }
     }
 }

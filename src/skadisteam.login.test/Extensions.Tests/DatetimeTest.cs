2724b355f915581dcf37d9b4cabd4c959c264b27
igeligel <kevinigeligeligel@gmail.com>
2016-07-06 15:43:24 +0200

    feat(test)
    add test for datetime extension

---
+++ src/skadisteam.login.test/Extensions.Tests/DatetimeTest.cs
@@ -0,0 +1,24 @@
+using skadisteam.login.Extensions;
+using System;
+using Xunit;
+
+namespace skadisteam.login.test
+{
+    public class DateTimeTest
+    {
+        [Fact]
+        public void CheckDate()
+        {
+            DateTime dateTime = new DateTime(2016, 5, 3, 15, 42, 47, DateTimeKind.Utc);
+            Assert.Equal(dateTime.ToUnixMilliSecondTime(), 1462290167000);
+        }
+
+        [Fact]
+        public void CheckInvalid()
+        {
+            DateTime dateTime = new DateTime(1963, 5, 3, 15, 42, 47, DateTimeKind.Utc);
+            var test = dateTime.ToUnixMilliSecondTime();
+            Assert.InRange(test, long.MinValue, -1);
+        }
+    }
+}

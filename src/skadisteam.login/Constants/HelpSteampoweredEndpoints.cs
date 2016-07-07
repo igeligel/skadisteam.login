eb62550c67e28e7145ddd246a1ee33fc78d23c5b
igeligel <kevinigeligeligel@gmail.com>
2016-07-07 23:03:57 +0200

    docs(constants)
    add xml documentation for helpsteampowered endpoint

--- src/skadisteam.login/Constants/HelpSteampoweredEndpoints.cs
+++ src/skadisteam.login/Constants/HelpSteampoweredEndpoints.cs
@@ -1,5 +1,8 @@
 namespace skadisteam.login.Constants
 {
+    /// <summary>
+    /// Class which contains constants for interacting with the help.steampowered endpoint.
+    /// </summary>
     internal class HelpSteampoweredEndpoints
     {
         internal const string TransferLogin = "/login/transfer";
